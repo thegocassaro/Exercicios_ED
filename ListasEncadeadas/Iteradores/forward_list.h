3adc9c5ed84fdc538b9f25789f97d5e7748dcb64
Gustavo <gustavo.cassaro@edu.ufes.br>
2024-08-21 11:02:02 -0300

    ListasEncadeadas

--- ListasEncadeadas/Iteradores/forward_list.h
+++ ListasEncadeadas/Iteradores/forward_list.h
@@ -3,13 +3,18 @@
 #define _FORWARD_forward_list_H_
 
 #include "node.h"
+#include <stdbool.h>
 
 typedef struct
 {
     Node *head;
-    Node *last;
+    Node* last;
     int size;
 } ForwardList;
+
+typedef struct ListIterator {
+    Node *current;
+} ListIterator;
 
 /**
  * @brief Construct a new Linked List:: Linked List object
@@ -43,17 +48,6 @@ int forward_list_size(ForwardList *l);
  *
  */
 void forward_list_push_front(ForwardList *l, data_type data);
-
-/**
- * @brief Pushes a new node to the back of the linked list.
- *  Allocates memory for a new node and inserts it at the front of the linked list.
- * @param l
- * Pointer to the linked list.
- * @param data
- * Pointer to the data to be stored in the new node.
- *
- */
-void forward_list_push_back(ForwardList *l, data_type data);
 
 /**
  * @brief Print the elements of the linked list.
@@ -152,5 +146,38 @@ void forward_list_sort(ForwardList *l);
  *
  */
 void forward_list_destroy(ForwardList *l);
+
+/**
+ * @brief Returns an iterator to the first node of the linked list.
+ * @param l
+ * Pointer to the linked list.
+ * @return ListIterator*
+ * Pointer to the iterator.
+ */
+ListIterator *list_iterator_construct(ForwardList *l);
+
+/**
+ * @brief Free the memory allocated to the iterator.
+ * @param it
+ * Pointer to the iterator.
+ */
+void list_iterator_destroy(ListIterator *it);
+
+/**
+ * @brief Returns the data stored in the node and updates the iterator to point to the next node.
+ * @param it
+ * Pointer to the iterator.
+ * @return data_type
+ * Pointer to the data stored in the current node.
+ */
+data_type* list_iterator_next(ListIterator *it);
+
+/**
+ * @brief Returns true if the iterator is over.
+ * @param it
+ * Pointer to the iterator.
+ * @return 1 if the iterator is over, and 0 otherwise.
+ */
+bool list_iterator_is_over(ListIterator *it);
 
 #endif
